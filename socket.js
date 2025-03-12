d79915dbf5e80a814ca063aa7b9da41de9e3aa95
jonathandestinec <jonathandestinec@gmail.com>
2025-03-12 18:22:17 +0100

    socket.io url

--- socket.js
+++ socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:4000", {
+export const socket = io("https://super-basic-chat-app.onrender.com", {
   transports: ["websocket"],
 });
