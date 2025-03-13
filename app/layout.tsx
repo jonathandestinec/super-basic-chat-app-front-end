23494e0c5aed6a41af5e6cf5950ab4cd8ec24576
jonathandestinec <jonathandestinec@gmail.com>
2025-03-13 10:47:45 +0100

    fix metada title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Super BAsi Chat App",
+  title: "Super Basic Chat App",
   description: "Just as it says",
 };
 
